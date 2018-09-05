823c55fbf58825b2df3da125e7c0543c5bb58dc7
zhuangrong2025 <42589963@qq.com>
2018-09-05 17:07:40 +0800

    点击按钮推送

--- socketio/index.js
+++ socketio/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require("express")
+var path = require("path")
+var app = require("express")()
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")))
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/broadcast.html")
+})
 
 // io.on('connection', function(socket){
 //   socket.on('chat message', function(msg){
@@ -16,12 +16,12 @@ app.get('/', function(req, res){
 //   })
 // });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+io.on("connection", function(socket){
+	socket.on("chat message", function(msg){
+		io.emit("chat message", msg)
+	})
+})
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+	console.log("listening on *:3000")
+})
